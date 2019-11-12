9e375b615d0cd4c251a4fdd852db3341cc5bff4b
jiangxf <limengcanyu@163.com>
2019-11-12 16:40:12 +0800

    add spring-boot-ssl

---
+++ spring-boot-ssl/README.md
@@ -0,0 +1,7 @@
+# spring-boot-ssl
+
+页面访问需使用 https://，例如
+https://localhost:8443/echo
+
+启动日志中会看到同时支持两个协议的日志
+--2019-11-12 16:34:51.887 - INFO 12516 --- [           main] o.s.b.w.embedded.tomcat.TomcatWebServer    92 : Tomcat initialized with port(s): 8443 (https) 8080 (http)
---
+++ spring-boot-ssl/src/main/java/com/spring/boot/ssl/configure/SslConfigure.java
@@ -0,0 +1,23 @@
+package com.spring.boot.ssl.configure;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SslConfigure {
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+        return tomcat;
+    }
+
+    private Connector createStandardConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(8080); // 设置HTTP协议端口
+        return connector;
+    }
+}
