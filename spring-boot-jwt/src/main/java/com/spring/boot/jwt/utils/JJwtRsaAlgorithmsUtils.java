13c580ac0b0509ab9c64bcee158f5080a466497f
jiangxf <limengcanyu@163.com>
2019-11-12 16:53:05 +0800

    update spring-boot-jwt

---
+++ spring-boot-jwt/src/main/java/com/spring/boot/jwt/utils/JJwtRsaAlgorithmsUtils.java
@@ -0,0 +1,47 @@
+package com.spring.boot.jwt.utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.impl.crypto.RsaProvider;
+
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+
+public class JJwtRsaAlgorithmsUtils {
+    private static PublicKey getPublicKey() {
+        int keySizeInBits = 4096;
+        SecureRandom random = new SecureRandom(JwtConstant.JWT_SECRET.getBytes());
+        KeyPair keyPair = RsaProvider.generateKeyPair(keySizeInBits, random);
+        return keyPair.getPublic();
+    }
+
+    private static PrivateKey getPrivateKey() {
+        int keySizeInBits = 4096;
+        SecureRandom random = new SecureRandom(JwtConstant.JWT_SECRET.getBytes());
+        KeyPair keyPair = RsaProvider.generateKeyPair(keySizeInBits, random);
+        return keyPair.getPrivate();
+    }
+
+    public static String creatJWS(Claims claims) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .signWith(getPrivateKey())
+                .compact();
+    }
+
+    public static Claims readJWS(String jwsString) {
+        try {
+            Jws<Claims> jws = Jwts.parser()
+                    .setSigningKey(getPublicKey())
+                    .parseClaimsJws(jwsString);
+            return jws.getBody();
+        } catch (JwtException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+}
