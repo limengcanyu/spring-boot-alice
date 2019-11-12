66658d39061c71d3ecd354a29d3d6362687556e5
jiangxf <limengcanyu@163.com>
2019-11-12 17:58:20 +0800

    update veu-element-alice-admin

--- veu-element-alice-admin/src/main/java/com/veu/element/alice/admin/controller/UploadController.java
+++ veu-element-alice-admin/src/main/java/com/veu/element/alice/admin/controller/UploadController.java
@@ -1,10 +1,13 @@
 package com.veu.element.alice.admin.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.ResourceUtils;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,14 +17,23 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 @RequestMapping("/upload")
 @RestController
 public class UploadController {
     private static final Logger logger = LoggerFactory.getLogger(UploadController.class);
 
-    @RequestMapping("/elUpload")
-    public String elUpload(@RequestParam("file") MultipartFile uploadFile, HttpServletRequest request) {
+    /**
+     * 上传文件
+     *
+     * @param uploadFile
+     * @param request
+     * @return
+     */
+    @RequestMapping("/elUploadFile")
+    public String elUploadFile(@RequestParam("file") MultipartFile uploadFile, HttpServletRequest request) {
         if (uploadFile.isEmpty()) {
             return "file is empty.";
         }
@@ -37,6 +49,27 @@ public class UploadController {
             uploadFile.transferTo(file);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+
+        return "上传文件成功！";
+    }
+
+    /**
+     * 上传文件内容
+     *
+     * @param sheetList
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    @RequestMapping("/elUploadContent")
+    public String elUploadContent(@RequestBody List<Map<String, Object>> sheetList) {
+        if (!CollectionUtils.isEmpty(sheetList)) {
+            sheetList.forEach(sheet -> {
+                logger.debug("sheetIndex: {} sheetName: {}", sheet.get("sheetIndex"), sheet.get("sheetName"));
+                List<String> headers = (List<String>) sheet.get("headers");
+                List<Map<String, Object>> results = (List<Map<String, Object>>) sheet.get("results");
+                logger.debug("headers: {} \nresults: {}", JSONObject.toJSONString(headers), JSONObject.toJSONString(results));
+            });
         }
 
         return "上传文件成功！";
