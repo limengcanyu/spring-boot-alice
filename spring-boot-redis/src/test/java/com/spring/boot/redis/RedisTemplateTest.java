c1e66032649bfea59e75530c0a67cdf8a82b83d5
jiangxf <limengcanyu@163.com>
2019-11-19 17:20:35 +0800

    update spring-boot-redis

--- spring-boot-redis/src/test/java/com/spring/boot/redis/RedisTemplateTest.java
+++ spring-boot-redis/src/test/java/com/spring/boot/redis/RedisTemplateTest.java
@@ -21,26 +21,48 @@ public class RedisTemplateTest {
     @Autowired
     private RedisTemplate<String, Object> redisTemplate;
 
-    @Resource(name="redisTemplate")
+    @Resource(name = "redisTemplate")
     private ValueOperations<String, Object> valueOps;
 
-    @Resource(name="redisTemplate")
+    @Resource(name = "redisTemplate")
     private ListOperations<String, Object> listOps;
 
-    @Resource(name="redisTemplate")
+    @Resource(name = "redisTemplate")
     private SetOperations<String, Object> setOps;
 
-    @Resource(name="redisTemplate")
+    @Resource(name = "redisTemplate")
     private ZSetOperations<String, Object> zSetOps;
 
-    @Resource(name="redisTemplate")
-    private  GeoOperations<String, Object> geoOps;
+    @Resource(name = "redisTemplate")
+    private GeoOperations<String, Object> geoOps;
 
 //    @Resource(name="redisTemplate")
 //    private  HyperLogLogOperations<String, Object> hllOps;
 
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    public RedisTemplateTest() {
+    }
+
     @Test
     public void test() {
-        System.out.println();
+        String key = "CalculateASynTax:" + "tenant_000001" + ":" + "company_000001" + ":" + "2019-11";
+        String requestId = "request_000001";
+        stringRedisTemplate.opsForValue().set(key, requestId);
+        key = stringRedisTemplate.opsForValue().get(key);
+        System.out.println(key);
+
+        key = "CalculateASynTax:" + "tenant_000001" + ":" + "company_000001" + ":" + "2019-12";
+        requestId = "request_000002";
+        stringRedisTemplate.opsForValue().set(key, requestId);
+        key = stringRedisTemplate.opsForValue().get(key);
+        System.out.println(key);
+
+        key = "CalculateASynTax:" + "tenant_000001" + ":" + "company_000001" + ":" + "2019-11";
+        requestId = "request_000003";
+        stringRedisTemplate.opsForValue().set(key, requestId);
+        key = stringRedisTemplate.opsForValue().get(key);
+        System.out.println(key);
     }
 }
