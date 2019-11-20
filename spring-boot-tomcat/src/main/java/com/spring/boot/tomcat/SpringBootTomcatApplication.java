79f4a0171394ad728a346e222321ed6a2ad55bb6
jiangxf <limengcanyu@163.com>
2019-11-20 18:32:08 +0800

    update spring-boot-tomcat add log

--- spring-boot-tomcat/src/main/java/com/spring/boot/tomcat/SpringBootTomcatApplication.java
+++ spring-boot-tomcat/src/main/java/com/spring/boot/tomcat/SpringBootTomcatApplication.java
@@ -1,11 +1,25 @@
 package com.spring.boot.tomcat;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringBootTomcatApplication {
+    private static final Logger logger = LoggerFactory.getLogger(SpringBootTomcatApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(SpringBootTomcatApplication.class);
+        logger.debug("application started...");
+    }
+
+    @RequestMapping("")
+    public String echo() {
+        logger.info("hello!");
+        return "hello";
     }
 }
