898f59522341ad7c88cb09c89de88ac7822f473c
jiangxf <limengcanyu@163.com>
2019-11-20 17:59:03 +0800

    update spring-boot-async add Scheduling

--- spring-boot-async/src/main/java/com/spring/boot/async/SpringBootAsyncApplication.java
+++ spring-boot-async/src/main/java/com/spring/boot/async/SpringBootAsyncApplication.java
@@ -3,15 +3,16 @@ package com.spring.boot.async;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
-// 启用异步支持
+@EnableScheduling
 @EnableAsync
 @SpringBootApplication
 public class SpringBootAsyncApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootAsyncApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAsyncApplication.class, args);
+    }
 
 }
