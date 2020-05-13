2e4453e1fa150e7dd3663000ae23ec7fe400edd8
jiangxf <limengcanyu@163.com>
2020-05-13 16:04:25 +0800

    update pom

--- spring-boot-jetty/src/main/java/com/spring/boot/jetty/SpringBootJettyApplication.java
+++ spring-boot-jetty/src/main/java/com/spring/boot/jetty/SpringBootJettyApplication.java
@@ -1,8 +1,6 @@
-package com.spring.boot.tomcat;
+package com.spring.boot.jetty;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,22 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @SpringBootApplication
-public class SpringBootTomcatApplication {
-    private static final Logger logger = LoggerFactory.getLogger(SpringBootTomcatApplication.class);
-
+public class SpringBootJettyApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootTomcatApplication.class);
-        logger.debug("application started...");
+        SpringApplication.run(SpringBootJettyApplication.class);
     }
 
     /**
-     * localhost:8070/echo
+     * localhost:8072/echo
      *
      * @return
      */
     @RequestMapping("/echo")
     public String echo() {
-        log.debug("this is tomcat app echo");
-        return "this is tomcat app";
+        log.debug("this is jetty app echo");
+        return "this is jetty app";
     }
 }
--- spring-boot-undertow/src/main/java/com/spring/boot/undertow/SpringBootUndertowApplication.java
+++ spring-boot-undertow/src/main/java/com/spring/boot/undertow/SpringBootUndertowApplication.java
@@ -1,8 +1,6 @@
-package com.spring.boot.tomcat;
+package com.spring.boot.undertow;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,22 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @SpringBootApplication
-public class SpringBootTomcatApplication {
-    private static final Logger logger = LoggerFactory.getLogger(SpringBootTomcatApplication.class);
-
+public class SpringBootUndertowApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootTomcatApplication.class);
-        logger.debug("application started...");
+        SpringApplication.run(SpringBootUndertowApplication.class);
     }
 
     /**
-     * localhost:8070/echo
+     * localhost:8071/echo
      *
      * @return
      */
     @RequestMapping("/echo")
     public String echo() {
-        log.debug("this is tomcat app echo");
-        return "this is tomcat app";
+        log.debug("this is undertow app echo");
+        return "this is undertow app";
     }
 }
