c4e07ed2f9f10c32de130190cc27054e404fef5c
limengcanyu <limengcanyu@163.com>
2020-05-14 21:59:21 +0800

    update spring-boot-elasticsearch

--- spring-boot-elasticsearch/src/test/java/com/spring/boot/elasticsearch/ElasticsearchTemplateTest.java
+++ spring-boot-elasticsearch/src/test/java/com/spring/boot/elasticsearch/ElasticsearchTemplateTest.java
@@ -1,10 +1,15 @@
 package com.spring.boot.elasticsearch;
 
-import org.junit.runner.RunWith;
+import com.spring.boot.elasticsearch.entity.Person;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.core.query.GetQuery;
+import org.springframework.data.elasticsearch.core.query.IndexQuery;
+import org.springframework.data.elasticsearch.core.query.IndexQueryBuilder;
 
 /**
  * <p>Description: </p>
@@ -12,9 +17,32 @@ import org.springframework.test.context.junit4.SpringRunner;
  * @author rock
  * date 2019/06/23
  */
-@RunWith(SpringRunner.class)
+@Slf4j
 @SpringBootTest
 public class ElasticsearchTemplateTest {
+
     @Autowired
-    private ElasticsearchTemplate elasticsearchTemplate;
+    private ElasticsearchRestTemplate elasticsearchRestTemplate;
+
+    @Test
+    public void save(){
+        Person person = new Person();
+        person.setId("cb7bef");
+        person.setFirstName("Sarah");
+        person.setLastName("Connor");
+
+        IndexQuery indexQuery = new IndexQueryBuilder()
+                .withId(person.getId())
+                .withObject(person)
+                .build();
+        String documentId = elasticsearchRestTemplate.index(indexQuery);
+        log.debug("documentId: {}", documentId);
+    }
+
+    @Test
+    public void findById() {
+        Person person = elasticsearchRestTemplate
+                .queryForObject(GetQuery.getById("cb7bef".toString()), Person.class);
+        log.debug("person: {}", person);
+    }
 }
