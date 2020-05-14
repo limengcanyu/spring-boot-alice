2032b5faab8b9d75235f6260f9777d10c1576f97
limengcanyu <limengcanyu@163.com>
2020-05-14 19:03:53 +0800

    add spring-boot-json

---
+++ spring-boot-json/src/main/java/com/spring/boot/json/SpringBootJsonApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.json;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJsonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJsonApplication.class, args);
+    }
+
+}
