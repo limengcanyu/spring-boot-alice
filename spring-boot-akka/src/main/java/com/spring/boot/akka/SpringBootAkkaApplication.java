cda86921d953f1925eec8fb30d6b77f971252597
Rock Jiang <limengcanyu@163.com>
2020-09-29 11:43:52 +0800

    add spring-boot-akka

---
+++ spring-boot-akka/src/main/java/com/spring/boot/akka/SpringBootAkkaApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.akka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAkkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAkkaApplication.class, args);
+    }
+
+}
