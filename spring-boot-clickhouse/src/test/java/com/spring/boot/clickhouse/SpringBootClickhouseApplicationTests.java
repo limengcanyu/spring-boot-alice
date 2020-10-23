625003fbe31e1d1b41474b17f521f971fd1ab522
Rock Jiang <limengcanyu@163.com>
2020-10-23 09:28:49 +0800

    add spring-boot-clickhouse

---
+++ spring-boot-clickhouse/src/test/java/com/spring/boot/clickhouse/SpringBootClickhouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.boot.clickhouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootClickhouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
