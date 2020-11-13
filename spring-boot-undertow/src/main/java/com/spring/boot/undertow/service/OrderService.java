74b81c293a600c6290c9de9bef7e20c869a6551a
Rock Jiang <limengcanyu@163.com>
2020-11-13 15:31:14 +0800

    upgrade spring-boot-undertow

--- spring-boot-undertow/src/main/java/com/spring/boot/undertow/service/OrderService.java
+++ spring-boot-undertow/src/main/java/com/spring/boot/undertow/service/OrderService.java
@@ -1,5 +1,7 @@
 package com.spring.boot.undertow.service;
 
 public interface OrderService {
+
     String getOrderNumber();
+
 }
---
+++ spring-boot-undertow/src/test/java/com/spring/boot/undertow/SpringBootUndertowApplicationTests.java
@@ -0,0 +1,24 @@
+package com.spring.boot.undertow;
+
+import com.spring.boot.undertow.service.ProjectConfigurationService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * <p>Description: RedisUtils Test</p>
+ *
+ * @author rock.jiang
+ * date 2019/06/20
+ */
+@SpringBootTest
+public class SpringBootUndertowApplicationTests {
+    @Autowired
+    private ProjectConfigurationService projectConfigurationService;
+
+    @Test
+    public void getCurrentCompanyName() {
+        System.out.println(projectConfigurationService.getCurrentCompanyName());
+    }
+
+}
