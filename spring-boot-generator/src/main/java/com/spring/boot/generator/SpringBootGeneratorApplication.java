60ca244b45c3827aefc3bd94fa17ffc6de611809
Rock Jiang <limengcanyu@163.com>
2020-11-21 10:16:01 +0800

    update spring-boot-generator

---
+++ spring-boot-generator/src/main/java/com/spring/boot/generator/SpringBootGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGeneratorApplication.class, args);
+	}
+
+}
---
+++ spring-boot-generator/src/test/java/com/spring/boot/generator/SpringBootGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.boot.generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
