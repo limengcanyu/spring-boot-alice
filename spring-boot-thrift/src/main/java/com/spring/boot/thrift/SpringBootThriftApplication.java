aebc941e9ac4a3cd6020749ecc019acb2dc2bc6a
Rock Jiang <limengcanyu@163.com>
2020-11-24 12:38:10 +0800

    add spring-boot-thrift

---
+++ spring-boot-thrift/src/main/java/com/spring/boot/thrift/SpringBootThriftApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.thrift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootThriftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootThriftApplication.class, args);
+	}
+
+}
