72b96d0e6eb4971bd0f7b4a27c8f299c316d8c45
Rock Jiang <limengcanyu@163.com>
2020-11-27 17:29:52 +0800

    update spring-boot-webflux

--- spring-boot-jwt/src/test/java/com/spring/boot/jwt/JJwtHsAlgorithmsUtilsTest.java
+++ spring-boot-jwt/src/test/java/com/spring/boot/jwt/JJwtHsAlgorithmsUtilsTest.java
@@ -1,5 +1,6 @@
 package com.spring.boot.jwt;
 
+import com.spring.boot.jwt.utils.JJwtHsUtils;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import org.junit.jupiter.api.Test;
@@ -10,14 +11,14 @@ public class JJwtHsAlgorithmsUtilsTest {
         Claims claims = Jwts.claims();
         claims.put("hello", "world");
 
-        String jwsString = JJwtHsAlgorithmsUtils.creatJWS(claims);
+        String jwsString = JJwtHsUtils.creatJWS(claims);
         System.out.println("jwsString: " + jwsString);
     }
 
     @Test
     public void readJWS() {
         String jwsString = "eyJhbGciOiJIUzI1NiJ9.eyJoZWxsbyI6IndvcmxkIn0.Rellwe3UMg5UohoGjKix_senmWGAVnCYsr0EhfxPCH0";
-        Claims claims = JJwtHsAlgorithmsUtils.readJWS(jwsString);
+        Claims claims = JJwtHsUtils.readJWS(jwsString);
         System.out.println("claims: " + claims);
     }
 }
--- spring-boot-webflux/src/test/java/com/spring/boot/webflux/SpringBootWebfluxApplicationTests.java
+++ spring-boot-webflux/src/test/java/com/spring/boot/webflux/SpringBootWebfluxApplicationTests.java
@@ -3,9 +3,16 @@ package com.spring.boot.webflux;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * https://projectreactor.io/docs/core/release/reference/index.html#getting
@@ -34,7 +41,7 @@ class SpringBootWebfluxApplicationTests {
      *
      */
     @Test
-    public void simpleCreation() {
+    void simpleCreation() {
         Flux<String> fewWords = Flux.just("Hello", "World");
         Flux<String> manyWords = Flux.fromIterable(words);
 
@@ -50,7 +57,7 @@ class SpringBootWebfluxApplicationTests {
      * Finally, we want to output an index for each distinct letter, which can be done using zipWith and range:
      */
     @Test
-    public void findingMissingLetter() {
+    void findingMissingLetter() {
         Flux<String> manyLetters = Flux
                 .fromIterable(words)
                 .flatMap(word -> Flux.fromArray(word.split("")))
@@ -62,4 +69,93 @@ class SpringBootWebfluxApplicationTests {
         manyLetters.subscribe(System.out::println);
     }
 
+
+    CompletableFuture<List<String>> ifhIds() {
+        return CompletableFuture.completedFuture(new ArrayList<>());
+    }
+
+    CompletableFuture<String> ifhName(String name){
+        return CompletableFuture.completedFuture("");
+    }
+
+    CompletableFuture<Integer> ifhStat(String stat){
+        return CompletableFuture.completedFuture(1);
+    }
+
+    /**
+     * Example of CompletableFuture combination
+     */
+    @Test
+    void futureCombination() {
+        CompletableFuture<List<String>> ids = ifhIds();
+
+        CompletableFuture<List<String>> result = ids.thenComposeAsync(l -> {
+            Stream<CompletableFuture<String>> zip = l.stream().map(i -> {
+                        CompletableFuture<String> nameTask = ifhName(i); // 根据id获取名称
+                        CompletableFuture<Integer> statTask = ifhStat(i); // 根据id获取stat
+
+                        // 将名称与stat任务的结果合在一起操作后返回
+                        return nameTask.thenCombineAsync(statTask, (name, stat) -> "Name " + name + " has stats " + stat);
+                    });
+
+            List<CompletableFuture<String>> combinationList = zip.collect(Collectors.toList());
+
+            CompletableFuture<String>[] combinationArray = combinationList.toArray(new CompletableFuture[combinationList.size()]);
+
+            CompletableFuture<Void> allDone = CompletableFuture.allOf(combinationArray);
+
+            return allDone.thenApply(v -> combinationList.stream()
+                    .map(CompletableFuture::join)
+                    .collect(Collectors.toList()));
+        });
+
+        List<String> results = result.join();
+        assertThat(results).contains(
+                "Name NameJoe has stats 103",
+                "Name NameBart has stats 104",
+                "Name NameHenry has stats 105",
+                "Name NameNicole has stats 106",
+                "Name NameABSLAJNFOAJNFOANFANSF has stats 121");
+
+    }
+
+    Flux<String> ifhrIds(){
+        return Flux.fromArray(new String[]{"1", "2"});
+    }
+
+    Mono<String> ifhrName(String id){
+        return Mono.just("1");
+    }
+
+    Mono<Integer> ifhrStat(String id){
+        return Mono.just(1);
+    }
+
+    /**
+     * Example of Reactor code equivalent to future code
+     */
+    @Test
+    void reactorCombination() {
+        Flux<String> ids = ifhrIds();
+
+        Flux<String> combinations =
+                ids.flatMap(id -> {
+                    Mono<String> nameTask = ifhrName(id);
+                    Mono<Integer> statTask = ifhrStat(id);
+
+                    return nameTask.zipWith(statTask,
+                            (name, stat) -> "Name " + name + " has stats " + stat);
+                });
+
+        Mono<List<String>> result = combinations.collectList();
+
+        List<String> results = result.block();
+        assertThat(results).containsExactly(
+                "Name NameJoe has stats 103",
+                "Name NameBart has stats 104",
+                "Name NameHenry has stats 105",
+                "Name NameNicole has stats 106",
+                "Name NameABSLAJNFOAJNFOANFANSF has stats 121"
+        );
+    }
 }
