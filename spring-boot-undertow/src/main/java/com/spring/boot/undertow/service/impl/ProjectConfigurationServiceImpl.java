748b49cadae33a1fbc360b9a0e0ce14b90f7aca1
Rock Jiang <limengcanyu@163.com>
2020-12-04 10:08:11 +0800

    update spring-boot-configuration-processor

--- spring-boot-undertow/src/main/java/com/spring/boot/undertow/service/impl/ProjectConfigurationServiceImpl.java
+++ spring-boot-undertow/src/main/java/com/spring/boot/undertow/service/impl/ProjectConfigurationServiceImpl.java
@@ -2,6 +2,7 @@ package com.spring.boot.undertow.service.impl;
 
 import com.spring.boot.undertow.service.ProjectConfigurationService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Service;
 
 @Service
