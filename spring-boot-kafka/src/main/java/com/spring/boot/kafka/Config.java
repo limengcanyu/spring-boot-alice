2d356044f0113d38831af49141d26f8db0eef09b
Rock Jiang <limengcanyu@163.com>
2020-12-10 09:02:10 +0800

    update spring-boot-kafka

--- spring-boot-kafka/src/main/java/com/spring/boot/kafka/Config.java
+++ spring-boot-kafka/src/main/java/com/spring/boot/kafka/Config.java
@@ -1,70 +1,70 @@
-package com.spring.boot.kafka;
-
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.*;
-import java.util.HashMap;
-import java.util.Map;
-
-/**
- * <p>Description: </p>
- *
- * @author rock.jxf
- * @date 2020/12/9 21:39
- */
-@Configuration
-@EnableKafka
-public class Config {
-
-    @Autowired
-    private EmbeddedKafkaBroker embeddedKafka;
-
-    @Bean
-    ConcurrentKafkaListenerContainerFactory<Integer, String> kafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<Integer, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory());
-        return factory;
-    }
-
-    @Bean
-    public ConsumerFactory<Integer, String> consumerFactory() {
-        return new DefaultKafkaConsumerFactory<>(consumerConfigs());
-    }
-
-    @Bean
-    public Map<String, Object> consumerConfigs() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, embeddedKafka.getBrokersAsString());
-//        ...
-        return props;
-    }
-
-    @Bean
-    public Listener listener() {
-        return new Listener();
-    }
-
-    @Bean
-    public ProducerFactory<Integer, String> producerFactory() {
-        return new DefaultKafkaProducerFactory<>(producerConfigs());
-    }
-
-    @Bean
-    public Map<String, Object> producerConfigs() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, embeddedKafka.getBrokersAsString());
-//        ...
-        return props;
-    }
-
-    @Bean
-    public KafkaTemplate<Integer, String> kafkaTemplate() {
-        return new KafkaTemplate<Integer, String>(producerFactory());
-    }
-
-}
+//package com.spring.boot.kafka;
+//
+//import org.apache.kafka.clients.consumer.ConsumerConfig;
+//import org.apache.kafka.clients.producer.ProducerConfig;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.kafka.annotation.EnableKafka;
+//import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+//import org.springframework.kafka.core.*;
+//import java.util.HashMap;
+//import java.util.Map;
+//
+///**
+// * <p>Description: </p>
+// *
+// * @author rock.jxf
+// * @date 2020/12/9 21:39
+// */
+//@Configuration
+//@EnableKafka
+//public class Config {
+//
+//    @Autowired
+//    private EmbeddedKafkaBroker embeddedKafka;
+//
+//    @Bean
+//    ConcurrentKafkaListenerContainerFactory<Integer, String> kafkaListenerContainerFactory() {
+//        ConcurrentKafkaListenerContainerFactory<Integer, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+//        factory.setConsumerFactory(consumerFactory());
+//        return factory;
+//    }
+//
+//    @Bean
+//    public ConsumerFactory<Integer, String> consumerFactory() {
+//        return new DefaultKafkaConsumerFactory<>(consumerConfigs());
+//    }
+//
+//    @Bean
+//    public Map<String, Object> consumerConfigs() {
+//        Map<String, Object> props = new HashMap<>();
+//        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, embeddedKafka.getBrokersAsString());
+////        ...
+//        return props;
+//    }
+//
+//    @Bean
+//    public Listener listener() {
+//        return new Listener();
+//    }
+//
+//    @Bean
+//    public ProducerFactory<Integer, String> producerFactory() {
+//        return new DefaultKafkaProducerFactory<>(producerConfigs());
+//    }
+//
+//    @Bean
+//    public Map<String, Object> producerConfigs() {
+//        Map<String, Object> props = new HashMap<>();
+//        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, embeddedKafka.getBrokersAsString());
+////        ...
+//        return props;
+//    }
+//
+//    @Bean
+//    public KafkaTemplate<Integer, String> kafkaTemplate() {
+//        return new KafkaTemplate<Integer, String>(producerFactory());
+//    }
+//
+//}
