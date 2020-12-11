f4bfe227667ccacae00bb4ced2e55f5b5928cc07
Rock Jiang <limengcanyu@163.com>
2020-12-11 16:20:37 +0800

    add spring-boot-caffeine

---
+++ spring-boot-caffeine/src/main/java/com/spring/boot/caffeine/SpringBootCaffeineApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.caffeine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCaffeineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCaffeineApplication.class, args);
+    }
+
+}
---
+++ spring-boot-caffeine/src/test/java/com/spring/boot/caffeine/SpringBootCaffeineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.boot.caffeine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCaffeineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
