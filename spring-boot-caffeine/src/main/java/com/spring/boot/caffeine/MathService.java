89530a52d42bbf7d85df33e014d5412371460818
Rock Jiang <limengcanyu@163.com>
2020-12-14 16:32:58 +0800

    update spring-boot-caffeine

---
+++ spring-boot-caffeine/src/main/java/com/spring/boot/caffeine/MathService.java
@@ -0,0 +1,15 @@
+package com.spring.boot.caffeine;
+
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MathService {
+
+    @Cacheable("piDecimals")
+    public int computePiDecimal(int i) {
+        // ...
+        return i + 1;
+    }
+
+}
