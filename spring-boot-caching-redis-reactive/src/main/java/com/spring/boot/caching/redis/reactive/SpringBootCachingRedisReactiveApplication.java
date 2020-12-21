d9c2d421e3f28861c0eb1f9ec1eae6e89167f007
Rock Jiang <limengcanyu@163.com>
2020-12-21 10:48:14 +0800

    update project name

---
+++ spring-boot-caching-redis-reactive/src/main/java/com/spring/boot/caching/redis/reactive/SpringBootCachingRedisReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.caching.redis.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCachingRedisReactiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCachingRedisReactiveApplication.class, args);
+    }
+
+}
---
+++ spring-boot-caching-redis-reactive/src/test/java/com/spring/boot/caching/redis/reactive/SpringBootCachingRedisReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.boot.caching.redis.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCachingRedisReactiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
