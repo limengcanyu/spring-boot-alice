82d97269b2a15ea07d49ad1637f2fc462289f979
Rock Jiang <limengcanyu@163.com>
2020-12-21 11:21:58 +0800

    add spring-boot-hazelcast

---
+++ spring-boot-hazelcast/src/main/java/com/spring/boot/hazelcast/SpringBootHazelcastApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.hazelcast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHazelcastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHazelcastApplication.class, args);
+    }
+
+}
---
+++ spring-boot-hazelcast/src/test/java/com/spring/boot/hazelcast/SpringBootHazelcastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.boot.hazelcast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHazelcastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
