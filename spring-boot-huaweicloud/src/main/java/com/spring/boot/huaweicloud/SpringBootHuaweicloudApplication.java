8ab03269af8ce4c387b803ca2f74882631c345ce
limengcanyu <limengcanyu@163.com>
2021-03-03 16:28:05 +0800

    update pom

---
+++ spring-boot-huaweicloud/src/main/java/com/spring/boot/huaweicloud/SpringBootHuaweicloudApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.huaweicloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHuaweicloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHuaweicloudApplication.class, args);
+    }
+
+}
---
+++ spring-boot-huaweicloud/src/test/java/com/spring/boot/huaweicloud/SpringBootHuaweicloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.boot.huaweicloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHuaweicloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
