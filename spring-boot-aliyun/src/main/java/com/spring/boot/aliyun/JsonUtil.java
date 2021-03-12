b9eca824550141364ad66ecc851eda2d1bc19806
limengcanyu <limengcanyu@163.com>
2021-03-12 12:44:52 +0800

    update spring-boot-huaweicloud

--- spring-boot-aliyun/src/main/java/com/spring/boot/aliyun/JsonUtil.java
+++ spring-boot-aliyun/src/main/java/com/spring/boot/aliyun/JsonUtil.java
@@ -4,6 +4,11 @@ import com.alibaba.fastjson.JSONObject;
 
 public class JsonUtil {
     public static JSONObject parse(Object object) {
+
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            
+        }
+        
         return null;
     }
 }
