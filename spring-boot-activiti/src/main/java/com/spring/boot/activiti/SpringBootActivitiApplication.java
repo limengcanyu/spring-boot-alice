00da94c4e2e85b65dee472c39bc66a9c0084b827
limengcanyu <limengcanyu@163.com>
2021-04-15 16:56:45 +0800

    add spring-boot-activiti

---
+++ spring-boot-activiti/src/main/java/com/spring/boot/activiti/SpringBootActivitiApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.activiti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootActivitiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootActivitiApplication.class, args);
+    }
+
+}
