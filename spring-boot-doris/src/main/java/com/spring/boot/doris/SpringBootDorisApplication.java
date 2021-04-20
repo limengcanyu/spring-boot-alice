347b10a62bfc645aa3d73adb01d1ab0e595b88a8
limengcanyu <limengcanyu@163.com>
2021-04-20 15:06:47 +0800

    add spring-boot-doris

---
+++ spring-boot-doris/src/main/java/com/spring/boot/doris/SpringBootDorisApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.doris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDorisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDorisApplication.class, args);
+    }
+
+}
