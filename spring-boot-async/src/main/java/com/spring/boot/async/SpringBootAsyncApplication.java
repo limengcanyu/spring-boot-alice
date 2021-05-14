483c80a07e9907052f04e64a77355d7c8ead5f92
limengcanyu <limengcanyu@163.com>
2021-05-14 14:58:37 +0800

    update spring-boot-async

--- spring-boot-async/src/main/java/com/spring/boot/async/SpringBootAsyncApplication.java
+++ spring-boot-async/src/main/java/com/spring/boot/async/SpringBootAsyncApplication.java
@@ -3,9 +3,11 @@ package com.spring.boot.async;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 
 //@EnableScheduling
+@EnableAsync
 @SpringBootApplication
 public class SpringBootAsyncApplication {
 
