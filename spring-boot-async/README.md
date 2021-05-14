bc61b805b628bd13dab21cc41ecd80c4d9e97b00
limengcanyu <limengcanyu@163.com>
2021-05-14 14:23:28 +0800

    update spring-boot-async

--- spring-boot-async/README.md
+++ spring-boot-async/README.md
@@ -18,3 +18,5 @@ http://localhost:8080/doWebAsyncTask
 2. 定义Bean，类中的方法使用 @Async 注解，Spring 会为该 Bean 使用 advice ，从而进行异步支持
 注： 在同一个类中调用 @Async 注解的方法时异步支持无效，原因是无法为自身类创建代理，所以必须在另一个类中定义异步方法，然后使用该类
 ``````
+
+spring默认任务池配置位置 spring.boot.autoconfigure 包中 TaskExecutionAutoConfiguration 类
--- spring-boot-async/src/main/java/com/spring/boot/async/SpringBootAsyncApplication.java
+++ spring-boot-async/src/main/java/com/spring/boot/async/SpringBootAsyncApplication.java
@@ -1,10 +1,8 @@
 package com.spring.boot.async;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.context.ApplicationContext;
 
 
 //@EnableScheduling
@@ -12,7 +10,13 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class SpringBootAsyncApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootAsyncApplication.class, args);
+        ApplicationContext context = SpringApplication.run(SpringBootAsyncApplication.class, args);
+        String[] beanDefinitionNames = context.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            if (beanDefinitionName.toLowerCase().contains("task")) {
+                System.out.println("beanDefinitionName: " + beanDefinitionName);
+            }
+        }
     }
 
 }
